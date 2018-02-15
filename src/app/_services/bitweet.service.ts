bf42d52f2efd85d46ed0cb90917b42fcaf06d0b8
aldbr <alex.811@hotmail.fr>
2018-02-15 13:38:38 +0100

    add bitweet get methods

--- src/app/_services/bitweet.service.ts
+++ src/app/_services/bitweet.service.ts
@@ -12,8 +12,19 @@ export class BitweetService {
     return this.http.get<Bitweet[]>('http://localhost/bitcorner/bitCorner/Server/index.php?action=getBitweets');
   }
 
-  //getBitweet(id: number): Observable<Bitweet>  {
-  //  return this.http.get<Bitweet>('http://localhost/bitcorner/bitCorner/Server/index.php?action=getBitweets');
-  //}
+  getBitweet(id: number): Observable<Bitweet>  {
+    id = 649;
+    return this.http.get<Bitweet>('http://localhost/bitcorner/bitCorner/Server/index.php?action=getBitweet&params={\"id\":'+id+'}');
+  }
+
+  getBitweetsFromUser(userId: number): Observable<Bitweet[]>  {
+    userId = 644;
+    return this.http.get<Bitweet[]>('http://localhost/bitcorner/bitCorner/Server/index.php?action=getBitweetsFromUser&params={\"idUser\":'+userId+'}');
+  }
+
+  getBitweetsFromChannel(channelId: number): Observable<Bitweet[]>  {
+    channelId = 647;
+    return this.http.get<Bitweet[]>('http://localhost/bitcorner/bitCorner/Server/index.php?action=getBitweetsFromChannel&params={\"idChannel\":'+channelId+'}');
+  }
 
 }
--- src/app/bitweet-newsfeed/bitweet-newsfeed.component.ts
+++ src/app/bitweet-newsfeed/bitweet-newsfeed.component.ts
@@ -13,9 +13,28 @@ export class BitweetNewsfeedComponent {
   constructor(private bitweetService: BitweetService) {}
   profile = {};
   bitweets: Bitweet[];
+  bitweet: Bitweet;
 
   loadBitweets() {
     this.bitweetService.getBitweets().subscribe(
+      data => { this.bitweets = data; },
+      error => { console.log(error); });
+  }
+
+  loadBitweet(id: number) {
+    this.bitweetService.getBitweet(id).subscribe(
+      data => { this.bitweet = data; },
+      error => { console.log(error); });
+  }
+
+  loadBitweetsFromUser(userId: number) {
+    this.bitweetService.getBitweetsFromUser(userId).subscribe(
+      data => { this.bitweets = data; },
+      error => { console.log(error); });
+  }
+
+  loadBitweetsFromChannel(channelId: number) {
+    this.bitweetService.getBitweetsFromChannel(channelId).subscribe(
       data => { this.bitweets = data; },
       error => { console.log(error); });
   }
